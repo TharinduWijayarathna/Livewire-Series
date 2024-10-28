367d72ced614e088c63dbe5ebeeb9791e951a3c4
TharinduWijayarathna <wikum.dev@gmail.com>
2024-10-28 18:57:02 +0530

    Structure Created

--- Livewire - 1/routes/web.php
+++ Livewire - 1/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\BaseController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BaseController::class, 'index'])->name('home');
